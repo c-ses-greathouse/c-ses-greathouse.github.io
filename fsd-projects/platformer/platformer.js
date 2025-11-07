b6ba5cfb456476e1972813a0539cbfc69c950cdb
Technical School <technicalschool@BossierSchools.Org>
2025-11-07 10:24:08 -0600

    Finished platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,32 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(750, 500, 150, 20, "#00383D");
+    createPlatform(450, 601, 150, 20, "#90CBC5");
+    createPlatform(600, 700, 150, 20, "#EDF6F9");
+    createPlatform(750, 700, 150, 20, "#FE5D9F");
+    createPlatform(900, 601, 150, 20, "#CC0052");
+    createPlatform(600, 500, 150, 20, "#4E0E29");
+    createPlatform(750, 250, 150, 20, "#00383D");
+    createPlatform(350, 360, 150, 20, "#90CBC5");
+    createPlatform(120, 240, 150, 20, "#EDF6F9");
+    createPlatform(1250, 240, 150, 20, "#FE5D9F");
+    createPlatform(1000, 360, 150, 20, "#CC0052");
+    createPlatform(600, 250, 150, 20, "#4E0E29");
 
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("database", 1310, 200);
+    createCollectable("database", 600, 650);
+    createCollectable("database", 700, 650);
+    createCollectable("database", 800, 650);
+    createCollectable("database", 900, 650);
     // TODO 4 - Create Cannons
-
+    createCannon("left", 550, 2000)
+    createCannon("top", 1050, 1000)
+    createCannon("right", 200, 2000)
 
     
     
